716e58d7230b8bdadadc674199bc214fafcebd76
Dominik Wójt <domin144@o2.pl>
2016-06-01 18:37:42 +0200

    Added license information.

--- src/Metamath_database_read_write.cc
+++ src/Metamath_database_read_write.cc
@@ -1,3 +1,22 @@
+/* 
+ * Copyright 2013 Dominik Wójt
+ * 
+ * This file is part of metamath_playground.
+ * 
+ * metamath_playground is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * metamath_playground is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with metamath_playground.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #include <stdexcept>
 #include <utility>
 
